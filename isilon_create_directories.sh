ef89f24e894de17c754648dded54195e552d06ae
Brian Radwanski <brian.radwanski@emc.com>
2016-07-14 04:54:20 +0000

    initial commit from v0.74 of old isilon-hadoop-tools from HSK.

---
+++ isilon_create_directories.sh
@@ -0,0 +1,360 @@
+#!/bin/bash
+###########################################################################
+##  Script to create Hadoop directory structure on Isilon.
+##  Must be run on Isilon system.
+###########################################################################
+
+if [ -z "$BASH_VERSION" ] ; then
+   # probably using zsh...
+   echo "Script not run from bash -- reinvoking under bash"
+   bash "$0"
+   exit $?
+fi
+
+declare -a ERRORLIST=()
+
+DIST=""
+FIXPERM="n"
+ZONE="System"
+CLUSTER_NAME=""
+
+#set -x
+
+function banner() {
+   echo "##################################################################################"
+   echo "## $*"
+   echo "##################################################################################"
+}
+
+function usage() {
+   echo "$0 --dist <cdh|hwx|phd|phd3|bi> [--zone <ZONE>] [--fixperm] [--append-cluster-name <clustername>]"
+   exit 1
+}
+
+function fatal() {
+   echo "FATAL:  $*"
+   exit 1
+}
+
+function warn() {
+   echo "ERROR:  $*"
+   ERRORLIST[${#ERRORLIST[@]}]="$*"
+}
+
+function yesno() {
+   [ -n "$1" ] && myPrompt=">>> $1 (y/n)? "
+   [ -n "$1" ] || myPrompt=">>> Please enter yes/no: "
+   read -rp "$myPrompt" yn
+   [ "z${yn:0:1}" = "zy" -o "z${yn:0:1}" = "zY" ] && return 0
+#   exit "DEBUG:  returning false from function yesno"
+   return 1
+}
+
+function makedir() {
+   if [ "z$1" == "z" ] ; then
+      echo "ERROR -- function makedir needs directory as an argument"
+   else
+      mkdir -p $1
+   fi
+}
+
+function fixperm() {
+   if [ "z$1" == "z" ] ; then
+      echo "ERROR -- function fixperm needs directory owner group perm as an argument"
+   else
+      uid=$(getUserUid $2)
+      gid=$(getGroupGid $3)
+      chown $uid $1
+      chown :$gid $1
+      chmod $4 $1
+   fi
+}
+
+function getHdfsRoot() {
+    local hdfsroot
+    #Check for Version to process correct syntax - isirad
+    if [ "`isi version|cut -c 15`" -lt 8 ]; then
+      hdfsroot=$(isi zone zones view $1 | grep "HDFS Root Directory:" | cut -f2 -d :)
+    else
+      hdfsroot=$(isi hdfs settings view --zone=$1 | grep "Root Directory:" | cut -f2 -d :)
+    fi
+    echo $hdfsroot
+}
+
+#Params: Username
+#returns: UID
+function getUserUid() {
+    local uid
+    uid=$(isi auth users view --zone $ZONE $1 | grep "  UID" | cut -f2 -d :)
+    echo $uid
+}
+
+#Params: GroupName
+#returns: GID
+function getGroupGid() {
+    local gid
+    gid=$(isi auth groups view --zone $ZONE $1 | grep "  GID:" | cut -f2 -d :)
+    echo $gid
+}
+
+
+if [ "`uname`" != "Isilon OneFS" ]; then
+   fatal "Script must be run on Isilon cluster as root."
+fi
+
+if [ "$USER" != "root" ] ; then
+   fatal "Script must be run as root user."
+fi
+
+# Parse Command-Line Args
+# Allow user to specify what functions to check
+while [ "z$1" != "z" ] ; do
+    # echo "DEBUG:  Arg loop processing arg $1"
+    case "$1" in
+      "--dist")
+             shift
+             DIST="$1"
+             echo "Info: Hadoop distribution:  $DIST"
+             ;;
+      "--zone")
+             shift
+             ZONE="$1"
+             echo "Info: will use users in zone:  $ZONE"
+             ;;
+      "--fixperm")
+             echo "Info: will fix permissions and owners on existing directories"
+             FIXPERM="y"
+             ;;
+      "--append-cluster-name")
+             shift
+             CLUSTER_NAME="-$1"
+             echo "Info: will add clustername to end of usernames: $CLUSTER_NAME"
+             ;;
+      *)     echo "ERROR -- unknown arg $1"
+             usage
+             ;;
+    esac
+    shift;
+done
+
+declare -a dirList
+
+case "$DIST" in
+    "cdh")
+        # Format is: dirname#perm#owner#group
+        dirList=(\
+            "/#755#hdfs#hadoop" \
+            "/hbase#755#hbase#hbase" \
+            "/solr#775#solr#solr" \
+            "/tmp#1777#hdfs#supergroup" \
+            "/tmp/logs#1777#mapred#hadoop" \
+            "/tmp/hive#777#hive#supergroup" \
+            "/user#755#hdfs#supergroup" \
+            "/user/history#777#mapred#hadoop" \
+            "/user/hive#775#hive#hive" \
+            "/user/hive/warehouse#1777#hive#hive" \
+            "/user/hue#755#hue#hue" \
+            "/user/hue/.cloudera_manager_hive_metastore_canary#777#hue#hue" \
+            "/user/impala#775#impala#impala" \
+            "/user/oozie#775#oozie#oozie" \
+            "/user/flume#775#flume#flume" \
+            "/user/spark#751#spark#spark" \
+            "/user/spark/applicationHistory#1777#spark#spark" \
+            "/user/sqoop2#775#sqoop2#sqoop" \
+            "/solr#775#solr#solr" \
+        )
+        ;;
+    "hwx")
+        # Format is: dirname#perm#owner#group
+        dirList=(\
+            "/#755#hdfs#hadoop" \
+            "/app-logs#777#yarn#hadoop#" \
+            "/app-logs/ambari-qa#770#ambari-qa#hadoop#" \
+            "/app-logs/ambari-qa/logs#770#ambari-qa#hadoop#" \
+            "/tmp#1777#hdfs#hdfs" \
+            "/apps#755#hdfs#hadoop#" \
+            "/apps/falcon#777#falcon#hdfs#" \
+            "/apps/accumulo/#750#accumulo#hadoop#" \
+            "/apps/hbase#755#hdfs#hadoop" \
+            "/apps/hbase/data#775#hbase#hadoop" \
+            "/apps/hbase/staging#711#hbase#hadoop" \
+            "/apps/hive#755#hdfs#hdfs" \
+            "/apps/hive/warehouse#777#hive#hdfs" \
+            "/apps/tez#755#tez#hdfs" \
+            "/apps/webhcat#755#hcat#hdfs" \
+            "/mapred#755#mapred#hadoop" \
+            "/mapred/system#755#mapred#hadoop" \
+            "/user#755#hdfs#hdfs" \
+            "/user/ambari-qa#770#ambari-qa#hdfs" \
+            "/user/hcat#755#hcat#hdfs" \
+            "/user/hdfs#755#hdfs#hdfs" \
+            "/user/hive#700#hive#hdfs" \
+            "/user/hue#755#hue#hue" \
+            "/user/oozie#775#oozie#hdfs" \
+            "/user/yarn#755#yarn#hdfs" \
+            "/system/yarn/node-labels#700#yarn#hadoop" \
+        )
+        ;;
+    "phd")
+        # Format is: dirname#perm#owner#group
+        dirList=(\
+            "/#755#hdfs#hadoop" \
+            "/apps#755#hdfs#hadoop#" \
+            "/apps/hbase#755#hdfs#hadoop" \
+            "/apps/hbase/data#775#hbase#hadoop" \
+            "/apps/hbase/staging#711#hbase#hadoop" \
+            "/hawq_data#770#gpadmin#hadoop"
+            "/hive#755#hdfs#hadoop" \
+            "/hive/gphd#755#hdfs#hadoop" \
+            "/hive/gphd/warehouse#1777#hive#hadoop" \
+            "/mapred#755#mapred#hadoop" \
+            "/mapred/system#700#mapred#hadoop" \
+            "/tmp#777#hdfs#hadoop" \
+            "/tmp/gphdtmp#777#hdfs#hadoop" \
+            "/user#777#hdfs#hadoop" \
+            "/user/history#777#mapred#hadoop" \
+            "/user/history/done#777#mapred#hadoop" \
+            "/user/history/done_intermediate#1777#mapred#hadoop" \
+            "/yarn#755#hdfs#hadoop" \
+            "/yarn/apps#777#mapred#hadoop" \
+        )
+        ;;
+    "phd3")
+        # Format is: dirname#perm#owner#group
+        dirList=(\
+            "/#755#hdfs#hadoop" \
+            "/app-logs#777#yarn#hadoop#" \
+            "/apps#755#hdfs#hadoop#" \
+            "/apps/hbase#755#hdfs#hadoop" \
+            "/apps/hbase/data#775#hbase#hadoop" \
+            "/apps/hbase/staging#711#hbase#hadoop" \
+            "/hawq_data#770#gpadmin#hadoop"
+            "/hive#755#hdfs#hadoop" \
+            "/hive/gphd#755#hdfs#hadoop" \
+            "/hive/gphd/warehouse#1777#hive#hadoop" \
+            "/mapred#755#mapred#hadoop" \
+            "/mapred/system#700#mapred#hadoop" \
+            "/mr-history#755#mapred#hadoop" \
+            "/tmp#1777#hdfs#hdfs" \
+            "/tmp/gphdtmp#777#hdfs#hadoop" \
+            "/user#777#hdfs#hadoop" \
+            "/user/ambari-qa#770#ambari-qa#hdfs" \
+            "/user/gpadmin#700#gpadmin#gpadmin" \
+            "/user/hbase#700#hbase#hbase" \
+            "/user/hcat#755#hcat#hdfs" \
+            "/user/history#777#mapred#hadoop" \
+            "/user/history/done#777#mapred#hadoop" \
+            "/user/history/done_intermediate#1777#mapred#hadoop" \
+            "/user/hive#700#hive#hdfs" \
+            "/user/hue#755#hue#hue" \
+            "/user/mapred#700#mapred#mapred" \
+            "/user/oozie#775#oozie#hdfs" \
+            "/user/spark#755#spark#spark" \
+            "/user/spark/applicationHistory#1777#spark#spark" \
+            "/user/tez#700#tez#tez" \
+            "/user/yarn#700#yarn#yarn" \
+            "/user/zookeeper#700#zookeeper#zookeeper" \
+            "/yarn#755#hdfs#hadoop" \
+            "/yarn/apps#777#mapred#hadoop" \
+        )
+        ;;
+    "bi")
+        # Format is: dirname#perm#owner#group
+        dirList=(\
+            "/#755#hdfs#hadoop" \
+            "/tmp#1777#hdfs#hadoop" \
+            "/user#755#hdfs#hadoop" \
+            "/iop#755#hdfs#hadoop" \
+            "/apps#755#hdfs#hadoop" \
+            "/app-logs#755#hdfs#hadoop" \
+            "/mapred#755#hdfs#hadoop" \
+            "/mr-history#755#hdfs#hadoop" \
+            "/user/ambari-qa#770#ambari-qa#hadoop" \
+            "/user/hcat#775#hcat#hadoop" \
+            "/user/hive#775#hive#hadoop" \
+            "/user/oozie#775#oozie#hadoop" \
+            "/user/yarn#775#yarn#hadoop" \
+            "/user/zookeeper#775#zookeeper#hadoop" \
+            "/user/uiuser#775#uiuser#hadoop" \
+            "/user/spark#775#spark#hadoop" \
+            "/user/sqoop#775#sqoop#hadoop" \
+            "/user/solr#775#solr#hadoop" \
+            "/user/nagios#775#nagios#hadoop" \
+            "/user/bigsheets#775#bigsheets#hadoop" \
+            "/user/bigsql#775#bigsql#hadoop" \
+            "/user/dsmadmin#775#dsmadmin#hadoop" \
+            "/user/flume#775#flume#hadoop" \
+            "/user/hbase#775#hbase#hadoop" \
+            "/user/knox#775#knox#hadoop" \
+            "/user/mapred#775#mapred#hadoop" \
+            "/user/bigr#775#bigr#hadoop" \
+            "/user/bighome#775#bighome#hadoop" \
+            "/user/tauser#775#tauser#hadoop" \
+        )
+        ;;
+    *)
+        echo "ERROR -- Invalid Hadoop distribution"
+        usage
+        ;;
+esac
+
+HDFSROOT=$(getHdfsRoot $ZONE)
+echo "Info: HDFS root dir is $HDFSROOT"
+
+if [ ! -d $HDFSROOT ] ; then
+   fatal "HDFS root $HDFSROOT does not exist!"
+fi
+
+# MAIN
+
+banner "Creates Hadoop directory structure on Isilon system HDFS."
+
+prefix=0
+# Cycle through directory entries comparing owner, group, perm
+# Sample output from "ls -dl"  command below
+# drwxrwxrwx    8 hdfs  hadoop  1024 Aug 26 03:01 /tmp
+
+for direntry in ${dirList[*]}; do
+   read -a specs <<<"$(echo $direntry | sed 's/#/ /g')"
+
+   if [[ ${specs[0]} == /user/* ]] ; then
+     IFS='/' read -a path <<<"${specs[0]}"
+     old_path="/user/${path[2]}"
+     new_path="/user/${path[2]}$CLUSTER_NAME"
+     specs[0]="${specs[0]/$old_path/$new_path}"
+   fi
+   specs[2]="${specs[2]}$CLUSTER_NAME"
+   specs[3]="${specs[3]}$CLUSTER_NAME"
+
+   echo "DEBUG: specs dirname ${specs[0]}; perm ${specs[1]}; owner ${specs[2]}; group ${specs[3]}"
+   ifspath=$HDFSROOT${specs[0]}
+   # echo "DEBUG:  ifspath = $ifspath"
+
+   #  Get info about directory
+   if [ ! -d $ifspath ] ; then
+      # echo "DEBUG:  making directory $ifspath"
+      makedir $ifspath
+      fixperm $ifspath ${specs[2]} ${specs[3]} ${specs[1]}
+   elif [ "$FIXPERM" == "y" ] ; then
+      # echo "DEBUG:  fixing directory perm $ifspath"
+      fixperm $ifspath ${specs[2]} ${specs[3]} ${specs[1]}
+   else
+      warn "Directory $ifspath exists but no --fixperm not specified"
+   fi
+
+done
+
+if [ "${#ERRORLIST[@]}" != "0" ] ; then
+   echo "ERRORS FOUND:"
+   i=0
+   while [ $i -lt ${#ERRORLIST[@]} ]; do
+      echo "ERROR:  ${ERRORLIST[$i]}"
+      i=$(($i + 1))
+   done
+   fatal "ERRORS FOUND making Hadoop admin directory structure -- please fix before continuing"
+   exit 1
+else
+   echo "SUCCESS -- Hadoop admin directory structure exists and has correct ownership and permissions"
+fi
+
+echo "Done!"
---
+++ isilon_create_users.sh
@@ -0,0 +1,303 @@
+#!/bin/bash
+###########################################################################
+##  Script to create Hadoop users on Isilon.
+##  Must be run on Isilon system as root.
+###########################################################################
+
+if [ -z "$BASH_VERSION" ] ; then
+   # probably using zsh...
+   echo "Script not run from bash -- reinvoking under bash"
+   bash "$0"
+   exit $?
+fi
+
+declare -a ERRORLIST=()
+
+DIST=""
+STARTUID=1000
+STARTGID=1000
+ZONE="System"
+CLUSTER_NAME=""
+
+#set -x
+
+function banner() {
+   echo "##################################################################################"
+   echo "## $*"
+   echo "##################################################################################"
+}
+
+function usage() {
+   echo "$0 --dist <cdh|hwx|phd|phd3|bi> [--startgid <GID>] [--startuid <UID>] [--zone <ZONE>] [--append-cluster-name <clustername>]"
+   exit 1
+}
+
+function fatal() {
+   echo "FATAL:  $*"
+   exit 1
+}
+
+function warn() {
+   echo "ERROR:  $*"
+   ERRORLIST[${#ERRORLIST[@]}]="$*"
+}
+
+function addError() {
+   ERRORLIST+=("$*")
+}
+
+function yesno() {
+   [ -n "$1" ] && myPrompt=">>> $1 (y/n)? "
+   [ -n "$1" ] || myPrompt=">>> Please enter yes/no: "
+   read -rp "$myPrompt" yn
+   [ "z${yn:0:1}" = "zy" -o "z${yn:0:1}" = "zY" ] && return 0
+#   exit ":  returning false from function yesno"
+   return 1
+}
+
+function uidInUse() {
+   isi auth users view --uid $1 --zone $2 > /dev/null 2>&1
+}
+
+function userExists() {
+   isi auth users view $1 --zone $2 > /dev/null 2>&1
+}
+
+function groupExists() {
+   isi auth groups view $1 --zone $2 > /dev/null 2>&1
+}
+
+function gidInUse() {
+   isi auth groups view --gid $1 --zone $2 > /dev/null 2>&1
+}
+
+function getUidFromUser() {
+   local uid
+   uid=$(isi auth users view $1 --zone $2 | awk '/^ *UID:/ {print $2}')
+   echo $uid
+}
+
+function getUserFromUid() {
+   local user
+   user=$(isi auth users view --uid $1 --zone $2 | head -1 | awk '/^ *Name:/ {print $2}')
+   echo $user
+}
+
+function getGidFromGroup() {
+   local gid
+   gid=$(isi auth groups view $1 --zone $2 | awk '/^ *GID:/ {print $2}')
+   echo $gid
+}
+
+function getGroupFromGid() {
+   local group
+   group=$(isi auth groups view --gid $1 --zone $2 | head -1 | awk '/^ *Name:/ {print $2}')
+   echo $group
+}
+
+function getHdfsRoot() {
+    local hdfsroot
+    #Check for Version to process correct syntax - isirad
+    if [ "`isi version|cut -c 15`" -lt 8 ]; then
+       hdfsroot=$(isi zone zones view $1 | grep "HDFS Root Directory:" | cut -f2 -d :)
+    else
+       hdfsroot=$(isi hdfs settings view --zone=$1 | grep "Root Directory:" | cut -f2 -d :)
+    fi
+    echo $hdfsroot
+}
+
+### MAIN   main()
+
+if [ "`uname`" != "Isilon OneFS" ]; then
+   fatal "Script must be run on Isilon cluster as root."
+fi
+
+if [ "$USER" != "root" ] ; then
+   fatal "Script must be run as root user."
+fi
+
+# Parse Command-Line Args
+# Allow user to specify what functions to check
+while [ "z$1" != "z" ] ; do
+    # echo "DEBUG:  Arg loop processing arg $1"
+    case "$1" in
+      "--dist")
+             shift
+             DIST="$1"
+             echo "Info: Hadoop distribution:  $DIST"
+             ;;
+      "--startuid")
+             shift
+             STARTUID="$1"
+             echo "Info: users will start at UID $STARTUID"
+             ;;
+      "--startgid")
+             shift
+             STARTGID="$1"
+             echo "Info: groups will start at GID $STARTGID"
+             ;;
+      "--zone")
+             shift
+             ZONE="$1"
+             echo "Info: will put users in zone:  $ZONE"
+             ;;
+      "--append-cluster-name")
+             shift
+             CLUSTER_NAME="-$1"
+             echo "Info: will add clustername to end of usernames: $CLUSTER_NAME"
+             ;;
+      *)
+             echo "ERROR -- unknown arg $1"
+             usage
+             ;;
+    esac
+    shift;
+done
+
+case "$DIST" in
+    "cdh")
+        SUPER_USERS="hdfs mapred yarn"
+        SUPER_GROUPS="hadoop supergroup"
+        REQUIRED_USERS="$SUPER_USERS flume hbase hive hue impala oozie sample solr spark sqoop2 anonymous nothdfs cmjobuser systest"
+        REQUIRED_GROUPS="$REQUIRED_USERS $SUPER_GROUPS sqoop"
+        ;;
+    "hwx")
+        SUPER_USERS="hdfs mapred yarn hbase storm falcon tracer"
+        SUPER_GROUPS="hadoop"
+        REQUIRED_USERS="$SUPER_USERS tez hive hcat oozie zookeeper ambari-qa flume hue accumulo hadoopqa sqoop anonymous spark mahout ranger kms atlas ams kafka"
+        REQUIRED_GROUPS="$REQUIRED_USERS $SUPER_GROUPS"
+        ;;
+    "phd")
+        SUPER_USERS="hdfs mapred hbase gpadmin hive yarn"
+        SUPER_GROUPS="hadoop"
+        REQUIRED_USERS="$SUPER_USERS"
+        REQUIRED_GROUPS="$REQUIRED_USERS $SUPER_GROUPS"
+        ;;
+    "phd3")
+        SUPER_USERS="hdfs mapred hbase gpadmin hive yarn"
+        SUPER_GROUPS="hadoop"
+        REQUIRED_USERS="$SUPER_USERS tez hcat oozie zookeeper ambari-qa pxf knox spark hue"
+        REQUIRED_GROUPS="$REQUIRED_USERS $SUPER_GROUPS"
+        ;;
+    "bi")
+        SUPER_USERS="hdfs hadoop mapred hbase knox uiuser dsmadmin bigsheets ambari-qa rrdcached hive yarn hcat bigsql tauser bigr flume nagios solr spark sqoop zookeeper oozie bighome"
+        SUPER_GROUPS="hadoop"
+        REQUIRED_USERS="$SUPER_USERS anonymous ams"
+        REQUIRED_GROUPS="$REQUIRED_USERS $SUPER_GROUPS"
+        ;;
+    *)
+    echo "ERROR -- Invalid Hadoop distribution"
+        usage
+        ;;
+esac
+
+HDFSROOT=$(getHdfsRoot $ZONE)
+echo "Info: HDFS root:  $HDFSROOT"
+passwdfile="$ZONE.passwd"
+echo "Info: passwd file: $passwdfile"
+echo "# use this file to add to the passwd file of your clients" | cat > $passwdfile
+grpfile="$ZONE.group"
+echo "Info: group file: $grpfile"
+echo "# use this file to add to the group file of your clients" | cat > $grpfile
+
+# set -x
+gid=$STARTGID
+for group in $REQUIRED_GROUPS; do
+    # echo "DEBUG:  GID=$gid"
+    group="$group$CLUSTER_NAME"
+    if groupExists $group $ZONE ; then
+       gid=$(getGidFromGroup $group $ZONE)
+       addError "Group $group already exists at gid $gid in zone $ZONE"
+    elif gidInUse $gid $ZONE ; then
+       group=$(getGroupFromGid $gid $ZONE)
+       addError "GID $gid already in use by group $group in zone $ZONE"
+    else
+       isi auth groups create $group --gid $gid --zone $ZONE
+       [ $? -ne 0 ] && addError "Could not create group $group with gid $gid in zone $ZONE"
+       echo "$group:x:$gid" | cat >> $grpfile
+       [ $? -ne 0 ] && addError "Could not create entry in group file stub $grpfile for $group with gid $gid"
+    fi
+    gid=$(( $gid + 1 ))
+done
+# set +x
+
+uid=$STARTUID
+for user in $REQUIRED_USERS; do
+    # echo "DEBUG:  UID=$uid"
+    user="$user$CLUSTER_NAME"
+    if userExists $user $ZONE ; then
+       uid=$(getUidFromUser $user $ZONE)
+       addError "User $user already exists at uid $uid in zone $ZONE"
+    elif uidInUse $uid $ZONE ; then
+       user=$(getUserFromUid $uid $ZONE)
+       addError "UID $uid already in use by user $user in zone $ZONE"
+    else
+       isi auth users create $user --uid $uid --primary-group $user --zone $ZONE --provider local --home-directory $HDFSROOT/user/$user
+       [ $? -ne 0 ] && addError "Could not create user $user with uid $uid in zone $ZONE"
+       gid=$(getGidFromGroup $user $ZONE)
+       echo "$user:x:$uid:$gid:hadoop-svc-account:/home/$user:/bin/bash" | cat >> $passwdfile
+       [ $? -ne 0 ] && addError "Could not create entry in passwd file stub $passwdfile for $user with uid $uid"
+    fi
+    uid=$(( $uid + 1 ))
+done
+# set +x
+
+for group in $SUPER_GROUPS; do
+    group="$group$CLUSTER_NAME"
+    sprgrp=`grep $group $grpfile`
+    [ $? -ne 0 ] && addError "Could not locate entry $group in group file stub $grpfile"
+    for user in $SUPER_USERS; do
+        user="$user$CLUSTER_NAME"
+        isi auth groups modify $group --add-user $user --zone $ZONE
+        [ $? -ne 0 ] && addError "Could not add user $user to $group group in zone $ZONE"
+        sprgrp="$sprgrp,$user"
+    done
+    sed -i .bak /$group/d $grpfile
+    echo $sprgrp | cat >> $grpfile
+done
+# set +x
+
+
+# Special cases
+case "$DIST" in
+    "cdh")
+        isi auth groups modify sqoop$CLUSTER_NAME --add-user sqoop2$CLUSTER_NAME --zone $ZONE
+        [ $? -ne 0 ] && addError "Could not add user sqoop2$CLUSTER_NAME to sqoop$CLUSTER_NAME group in zone $ZONE"
+        sqp=`grep sqoop2$CLUSTER_NAME $grpfile`
+        sed -i .bak /$sqp/d $grpfile
+        echo "$sqp,sqoop" | cat >> $grpfile
+        [ $? -ne 0 ] && addError "Could not add user sqoop2$CLUSTER_NAME to sqoop$CLUSTER_NAME group in $grpfile"
+        ;;
+    "bi")
+        isi auth groups modify users$CLUSTER_NAME --add-user hive$CLUSTER_NAME --zone $ZONE
+        [ $? -ne 0 ] && addError "Could not add user hive$CLUSTER_NAME to users$CLUSTER_NAME group in zone $ZONE"
+        isi auth groups modify hcat$CLUSTER_NAME --add-user hive$CLUSTER_NAME --zone $ZONE
+        [ $? -ne 0 ] && addError "Could not add user hive$CLUSTER_NAME to hcat$CLUSTER_NAME group in zone $ZONE"
+        hct=`grep hcat$CLUSTER_NAME: $grpfile`
+        sed -i .bak /$hct/d $grpfile
+        echo "$hct,hive$CLUSTER_NAME" | cat >> $grpfile
+        [ $? -ne 0 ] && addError "Could not add user hive$CLUSTER_NAME to hcat$CLUSTER_NAME group in $grpfile"
+        isi auth groups modify knox$CLUSTER_NAME --add-user kafka$CLUSTER_NAME --zone $ZONE
+        [ $? -ne 0 ] && addError "Could not add user kafka$CLUSTER_NAME to knox$CLUSTER_NAME group in zone $ZONE"
+        knx=`grep knox$CLUSTER_NAME: $grpfile`
+        sed -i .bak /$knx/d $grpfile
+        echo "$knx,kafka$CLUSTER_NAME" | cat >> $grpfile
+        [ $? -ne 0 ] && addError "Could not add user kafka$CLUSTER_NAME to knox$CLUSTER_NAME group in $grpfile"
+        ;;
+esac
+
+### Deliver Results
+if [ "${#ERRORLIST[@]}" != "0" ] ; then
+   echo "ERRORS FOUND:"
+   i=0
+   while [ $i -lt ${#ERRORLIST[@]} ]; do
+      echo "*  ERROR:  ${ERRORLIST[$i]}"
+      i=$(( $i + 1 ))
+   done
+   fatal "ERRORS FOUND making Hadoop users in zone $ZONE -- please fix before continuing"
+   exit 1
+else
+   echo "SUCCESS -- Hadoop users created successfully!"
+fi
+
+echo "Done!"
