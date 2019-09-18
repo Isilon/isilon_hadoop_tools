ff0ce20217ad25beaa7f04e6af4abbf56f017c88
Boni Bruno <bonibruno@users.noreply.github.com>
2019-09-18 12:51:59 -0700

    Added needed directory to support YARN in CDH-6.3
    MR framework jars are currently in /user/yarn/mapreduce/mr-framework, added this directory to script as YARN failed to startup without it.  May need to add other service directories in /user/yarn down the road, for now, this was the only core service not starting.

--- isilon_create_directories.sh
+++ isilon_create_directories.sh
@@ -173,6 +173,7 @@ case "$DIST" in
             "/user/spark#751#spark#spark" \
             "/user/spark/applicationHistory#1777#spark#spark" \
             "/user/sqoop2#775#sqoop2#sqoop" \
+            "/user/yarn/mapreduce/mr-framework#755#yarn#hadoop" \
         )
         ;;
     "hwx")
