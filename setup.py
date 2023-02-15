908afaa10831aa1b1e39b792dd85b465de2734f1
Lars Francke <lars.francke@gmail.com>
2023-02-15 08:36:49 +0100

    Raise minimum Python requirement to 3.7
    This removes 3.5 and 3.6 as they are EOL: https://endoflife.date/python

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages("src"),
     include_package_data=True,
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,!=3.6.*",
     setup_requires=["setuptools_scm ~= 3.3"],
     install_requires=[
         'enum34 >= 1.1.6; python_version<"3.4"',
@@ -49,8 +49,6 @@ setuptools.setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
