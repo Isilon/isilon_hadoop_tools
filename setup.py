515649bddb11706c02f57db74713ff10f49324dc
David Tucker <david.tucker@emc.com>
2026-07-21 20:38:24 +0000

    Drop support for Python 3.9

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages("src"),
     include_package_data=True,
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=[
         "isi-sdk-7-2 ~= 0.2.11",
         "isi-sdk-8-0 ~= 0.2.11",
@@ -40,7 +40,6 @@ setuptools.setup(
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
